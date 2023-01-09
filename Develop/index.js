c579533f22446e8d54397538ec2ec8163fbf5145
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-09 18:17:29 -0500

    updated questions for ReadMe generator, connected inquirer, json, and markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,50 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: "input", 
+       name: "title", 
+       message: "What is the title of your project?"
+    },
+    {
+        type: "input", 
+        name: "description", 
+        message: "What description of your project?"
+    },
+    {
+        type: "input", 
+        name: "motivation", 
+        message: "What was the motivation for your project?"
+    },
+    {
+        type: "input", 
+        name: "solution", 
+        message: "What problem are you trying to solve with your project?"
+    },
+    {
+        type: "input", 
+        name: "learned", 
+        message: "What did you learn while making your project?"
+    },
+    {
+        type: "input", 
+        name: "unique", 
+        message: "What makes your project unique?"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log(generateMarkdown(answers))
+      });
+}
 
 // Function call to initialize app
 init();
